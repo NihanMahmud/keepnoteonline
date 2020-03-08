f358410d79a6aefadccba93bdf7cbfaaa6385328
NihanMahmud <61738463+NihanMahmud@users.noreply.github.com>
2020-03-08 17:48:02 +0600

    Update

--- Application.py
+++ Application.py
@@ -1,12 +1,4 @@
 from flask import Flask 
-from flask import render_template
-from flask import request
-from datetime import datetime as dt
-import os
-import re
-
-now=dt.today()
-info={}
 
 app=Flask(__name__)
 @app.route('/')
